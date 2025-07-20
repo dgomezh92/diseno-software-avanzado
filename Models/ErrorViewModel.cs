a7090a18687147e543b2aa45efbcd7f192d2b019
Diego Gomez <dgomezh_92@outlook.com>
2025-07-20 13:56:39 -0500

    creacion del proyecto MVC con git ignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Diseño_avanzado.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
